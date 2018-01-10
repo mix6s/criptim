df2718273e8ea776aeaf8b52290839a6e77fce71
mix6s <mian>
2018-01-10 22:08:59 +0300

    Implement Transfer usecase

---
+++ src/Domain/UseCase/TransferInvestorBitMoneyFromMainToTradingAccount/Request.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mix6s
+ * Date: 10.01.18
+ * Time: 22:02
+ */
+
+namespace Domain\UseCase\TransferInvestorBitMoneyFromMainToTradingAccount;
+
+
+class Request
+{
+
+}
