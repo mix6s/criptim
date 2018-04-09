d95800636434b75e23e42067f6955d762e8e6545
mix6s <mix6s@forbet.net>
2018-04-09 14:06:21 +0300

    Fix invite logic

--- src/FintobitBundle/Controller/InviteController.php
+++ src/FintobitBundle/Controller/InviteController.php
@@ -6,6 +6,7 @@ namespace FintobitBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class InviteController extends Controller
@@ -21,5 +22,38 @@ class InviteController extends Controller
 			return $this->redirectToRoute('fintobit.profile.index');
 		}
 		return $this->render('@Fintobit/Invite/index.html.twig');
+	}
+
+	/**
+	 * @Route("/invite/success", name="fintobit.invite.success")
+	 * @return Response
+	 */
+	public function successAction(): Response
+	{
+		if ($this->getUser()) {
+			return $this->redirectToRoute('fintobit.profile.index');
+		}
+		return $this->render('@Fintobit/Invite/success.html.twig');
+	}
+
+	/**
+	 * @Route("/invite/submit", name="fintobit.invite.submit", methods={"POST"})
+	 * @return Response
+	 */
+	public function submitAction(Request $request): Response
+	{
+		$email = $request->request->get('email');
+		if (empty($email)) {
+			return $this->redirectToRoute('fintobit.invite.success');
+		}
+		$message = (new \Swift_Message('Invite request'))
+			->setFrom('noreply@fintobit.com')
+			->setTo('fintobit@yandex.com')
+			->setBody(
+				$this->renderView('@Fintobit/Invite/email.html.twig', ['email' => $email]),
+				'text/html'
+			);
+		$this->get('swiftmailer.mailer')->send($message);
+		return $this->redirectToRoute('fintobit.invite.success');
 	}
 }
